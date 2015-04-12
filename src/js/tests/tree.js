1c413da61253947cb531942cdc955feb776c0416
Raymond Zhou <somecrazyidiot@gmail.com>
2015-04-12 02:10:54 -0400

    tree: Move test branch generation to Tree constructor.

--- src/js/tests/tree.js
+++ src/js/tests/tree.js
@@ -2,12 +2,7 @@ import THREE from 'three';
 
 import { PI2 } from './../math';
 
-import Tree, {
-  Base,
-  TrapezoidalPrism,
-  EquilateralTriangularPrism,
-  Pyramid
-} from './../tree/tree';
+import Tree from './../tree/tree';
 
 function render3d() {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -28,91 +23,13 @@ function render3d() {
   light.position.set( 0, 4, 4 );
   scene.add( light );
 
-  // Geometry.
-  const base = new Base( 2 );
-  const trapezoid = new TrapezoidalPrism( 1, 1.5, 1.5 );
-  const triangle = new EquilateralTriangularPrism( 1, 1 );
-
-  const pyramidRight = new Pyramid( 5 );
-
-  const trapezoidLeft = new TrapezoidalPrism( 0.75, 1.5, 0.5 );
-  const triangleLeft = new EquilateralTriangularPrism( 1, 1 );
-  const pyramidLeftLeft = new Pyramid( 2 );
-  const pyramidLeftRight = new Pyramid( 3 );
-
-  base.add( trapezoid );
-  trapezoid.add( triangle );
-  triangle.add( trapezoidLeft, 'left' );
-  triangle.add( pyramidRight, 'right' );
-  trapezoidLeft.add( triangleLeft );
-  triangleLeft.add( pyramidLeftLeft, 'left' );
-  triangleLeft.add( pyramidLeftRight, 'right' );
-
-  const geometry = new THREE.Geometry();
-  geometry.bones = [];
-
-  base.traverse( object => {
-    const offset = geometry.vertices.length;
-    const tempGeometry = object.createGeometry( offset );
-    geometry.vertices.push( ...tempGeometry.vertices );
-    geometry.faces.push( ...tempGeometry.faces );
-    object.createBone( geometry );
-  });
-
-  geometry.computeFaceNormals();
-  geometry.computeVertexNormals();
-
-  const material = new THREE.MeshPhongMaterial({
-    // wireframe: true,
-    skinning: true,
-    shading: THREE.FlatShading
-  });
-
-  const mesh = new THREE.SkinnedMesh( geometry, material );
+  const tree = new Tree();
+  const { mesh } = tree;
   scene.add( mesh );
 
   const skeletonHelper = new THREE.SkeletonHelper( mesh );
   skeletonHelper.material.linewidth = 4;
   mesh.add( skeletonHelper );
-
-  // Traverse bones and determine start heading angle.
-  const vector = new THREE.Vector3();
-
-  mesh.skeleton.bones.forEach( bone => {
-    bone.startAngle = 0;
-
-    const { parent } = bone;
-    if ( !parent ) {
-      return;
-    }
-
-    const grandparent = parent.parent;
-    if ( !grandparent ) {
-      return;
-    }
-
-    /*
-      (x, y)
-        o
-          \
-           \  (x1, y1)
-            o
-            |
-            |
-            o (x0, y0)
-     */
-
-    const { x, y } = vector.setFromMatrixPosition( bone.matrixWorld );
-    const { x: x1, y: y1 } = vector.setFromMatrixPosition( parent.matrixWorld );
-    const { x: x0, y: y0 } = vector.setFromMatrixPosition( grandparent.matrixWorld );
-
-    const cross = ( x - x0 ) * ( y1 - y0 ) - ( y - y0 ) * ( x1 - x0 );
-
-    // Left.
-    if ( cross < 0 ) { bone.startAngle = -Math.PI / 3; }
-    // Right.
-    if ( cross > 0 ) { bone.startAngle =  Math.PI / 3; }
-  });
 
   function animate() {
     // Rotate camera instead of mesh to prevent problems with SkeletonHelper
--- src/js/tree/tree.js
+++ src/js/tree/tree.js
@@ -510,22 +510,86 @@ export class Pyramid extends THREE.Object3D {
 
 export default class Tree {
   constructor() {
-    this.geometry = new THREE.Geometry();
-    this.geometry.bones = [
-      {
-        parent: -1,
-        name: 'root',
-        pos: [ 0, 0, 0 ],
-        rotq: [ 0, 0, 0, 1 ]
-      }
-    ];
+    const geometry = new THREE.Geometry();
+    geometry.bones = [];
 
-    this.material = new THREE.MeshBasicMaterial({
-      skinning: true,
-      wireframe: true
+    const base = new Base( 2 );
+    const trapezoid = new TrapezoidalPrism( 1, 1.5, 1.5 );
+    const triangle = new EquilateralTriangularPrism( 1, 1 );
+
+    const pyramidRight = new Pyramid( 5 );
+
+    const trapezoidLeft = new TrapezoidalPrism( 0.75, 1.5, 0.5 );
+    const triangleLeft = new EquilateralTriangularPrism( 1, 1 );
+    const pyramidLeftLeft = new Pyramid( 2 );
+    const pyramidLeftRight = new Pyramid( 3 );
+
+    base.add( trapezoid );
+    trapezoid.add( triangle );
+    triangle.add( trapezoidLeft, 'left' );
+    triangle.add( pyramidRight, 'right' );
+    trapezoidLeft.add( triangleLeft );
+    triangleLeft.add( pyramidLeftLeft, 'left' );
+    triangleLeft.add( pyramidLeftRight, 'right' );
+
+    base.traverse( object => {
+      const offset = geometry.vertices.length;
+      const tempGeometry = object.createGeometry( offset );
+      geometry.vertices.push( ...tempGeometry.vertices );
+      geometry.faces.push( ...tempGeometry.faces );
+      object.createBone( geometry );
     });
 
-    this.mesh = new THREE.SkinnedMesh( this.geometry, this.material );
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+
+    const material = new THREE.MeshPhongMaterial({
+      skinning: true,
+      shading: THREE.FlatShading
+    });
+
+    const mesh = new THREE.SkinnedMesh( geometry, material );
+
+    // Traverse bones and determine start heading angle.
+    mesh.skeleton.bones.forEach( bone => {
+      bone.startAngle = 0;
+
+      const { parent } = bone;
+      if ( !parent ) {
+        return;
+      }
+
+      const grandparent = parent.parent;
+      if ( !grandparent ) {
+        return;
+      }
+
+      /*
+        (x, y)
+          o
+            \
+             \  (x1, y1)
+              o
+              |
+              |
+              o (x0, y0)
+       */
+
+      const { x, y } = vector.setFromMatrixPosition( bone.matrixWorld );
+      const { x: x1, y: y1 } = vector.setFromMatrixPosition( parent.matrixWorld );
+      const { x: x0, y: y0 } = vector.setFromMatrixPosition( grandparent.matrixWorld );
+
+      const cross = ( x - x0 ) * ( y1 - y0 ) - ( y - y0 ) * ( x1 - x0 );
+
+      // Left.
+      if ( cross < 0 ) { bone.startAngle = -Math.PI / 3; }
+      // Right.
+      if ( cross > 0 ) { bone.startAngle =  Math.PI / 3; }
+    });
+
+    this.geometry = geometry;
+    this.material = material;
+    this.mesh = mesh;
   }
 
   update() {}
